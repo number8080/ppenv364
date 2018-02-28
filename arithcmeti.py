68f0cde89125d12de1ce01875547e6615b4ee7ee
root <root@localhost.localdomain>
2018-02-28 14:12:04 +0800

    Initial commit

---
+++ arithcmeti.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding:utf8 -*-
+
+num1 = int(input("1:"))
+num2 = int(input("2:"))
+#
+# num1 = input("1:").strip()
+# num2 = input("2:").strip()
+
+print("%s + %s = %s" %(num1,num2,num1 + num2))
+print("%s - %s = %s" %(num1,num2,num1 - num2))
+print("%s * %s = %s" %(num1,num2,num1 * num2))
+print("%s / %s = %s" %(num1,num2,num1 / num2))
+print("%s %% %s = %s" %(num1,num2,num1 % num2))
